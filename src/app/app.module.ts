7b936bb18e46be3960949fc6874f788abd34ed82
Sven <sven@mutzl.org>
2017-12-07 08:55:16 +0000

    Prevent language file caching on version update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,7 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { HttpModule, Http } from '@angular/http';
 import { IonicStorageModule } from '@ionic/storage';
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
-
-
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-
 import { MyApp } from './app.component';
 import { AppGlobals } from './app.global';
 import { AccountPage } from '../pages/account/account';
@@ -23,17 +20,16 @@ import { InformationPage } from '../pages/information/information';
 import { SettingsPage } from '../pages/settings/settings';
 import { DepositPage } from '../pages/deposit/deposit';
 import { PassphrasePage } from '../pages/passphrase/passphrase';
-
-
 import { GenerateKeyPage } from '../pages/generate-key/generate-key';
-
 import { ClipboardModule } from 'ngx-clipboard/dist';
 import { QRCodeModule } from 'angular2-qrcode';
 import { MvsServiceProvider } from '../providers/mvs-service/mvs-service';
 import { WalletServiceProvider } from '../providers/wallet-service/wallet-service';
 
+var pckg = require('../../package.json');
+
 export function HttpLoaderFactory(http: Http) {
-    return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+    return new TranslateHttpLoader(http, './assets/i18n/', '.json?v='+pckg.version);
 }
 
 @NgModule({
