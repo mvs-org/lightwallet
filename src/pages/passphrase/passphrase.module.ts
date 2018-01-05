a22a25d7e63df0d955271f2d27ba6d691680d27d
Laurent Salou <laurent.salou@hotmail.fr>
2018-01-05 17:57:58 +0800

    Fixed transition from Passphrase page to Account page (import from Backup words for mobile)

---
+++ src/pages/passphrase/passphrase.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { PassphrasePage } from './passphrase'
+import { TranslateModule} from '@ngx-translate/core';
+
+@NgModule({
+    declarations: [
+        PassphrasePage,
+    ],
+    imports: [
+        IonicPageModule.forChild(PassphrasePage),TranslateModule
+    ],
+    entryComponents: [
+    ],
+    exports: [
+        PassphrasePage
+    ]
+})
+export class PassphrasePageModule { }
