ef611ff92871cdf699d607a072e7cdb7b8d07c2e
Laurent Salou <laurent.salou@hotmail.fr>
2018-01-11 17:17:06 +0800

    Login mobile from QR Code ask the Password on the same page

--- src/pages/import-wallet-mobile/import-wallet-mobile.ts
+++ src/pages/import-wallet-mobile/import-wallet-mobile.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController, LoadingController, Loading } from 'ionic-angular';
 import { MvsServiceProvider } from '../../providers/mvs-service/mvs-service';
 import { AppGlobals } from '../../app/app.global';
 import { TranslateService } from '@ngx-translate/core';
@@ -13,16 +13,22 @@ import { WalletServiceProvider } from '../../providers/wallet-service/wallet-ser
 })
 export class ImportWalletMobilePage {
 
-    selectedFiles;
+    loading: Loading;
+    qrCodeLoaded: boolean
+    seed: string
 
-    constructor(public nav: NavController, private globals: AppGlobals, public navParams: NavParams, private mvs: MvsServiceProvider, private wallet: WalletServiceProvider, private alertCtrl: AlertController, private translate: TranslateService, private barcodeScanner: BarcodeScanner) {
+    constructor(public nav: NavController, private globals: AppGlobals, public navParams: NavParams, private mvs: MvsServiceProvider, private wallet: WalletServiceProvider, private alertCtrl: AlertController, private translate: TranslateService, private barcodeScanner: BarcodeScanner, private loadingCtrl: LoadingController) {
+        this.qrCodeLoaded = false;
     }
+
+    passwordValid = (password) => (password) ? password.length > 5 : false;
 
     scan() {
         let wallet = {};
         this.barcodeScanner.scan({ formats: 'QR_CODE' }).then((result) => {
             if (!result.cancelled) {
                 let content = result.text.toString().split('&')
+                this.seed = content[0]
                 if (content.length != 3)
                     this.showError('IMPORT_FILE')
                 else if (content[1] != this.globals.network.charAt(0))
@@ -30,13 +36,14 @@ export class ImportWalletMobilePage {
                 else
                     wallet = { "index": content[2] }
                     this.wallet.setWallet(wallet)
-                    this.wallet.setMobileWallet(content[0]).then(() => this.showPrompt(content[0]))
+                    this.wallet.setMobileWallet(content[0]).then(() => this.qrCodeLoaded = true)
 
             }
         })
     }
 
     decrypt(password, seed) {
+        this.showLoading()
         this.wallet.setMobileWallet(seed)
             .then(() => Promise.all([this.wallet.getWallet(password), this.wallet.getAddressIndex()]))
             .then((results) => this.mvs.generateAddresses(results[0], 0, results[1]))
@@ -50,47 +57,21 @@ export class ImportWalletMobilePage {
 
     howToMobile = () => this.nav.push("HowToMobilePage")
 
-    showPrompt(seed) {
-        this.translate.get('PASSWORD').subscribe((txt_password: string) => {
-            this.translate.get('CANCEL').subscribe((txt_cancel: string) => {
-                this.translate.get('ENTER_PASSWORD_HEADLINE_MOBILE').subscribe((txt_headline: string) => {
-                    this.translate.get('ENTER').subscribe((txt_enter: string) => {
-
-                        const alert = this.alertCtrl.create({
-                            title: txt_headline,
-                            inputs: [
-                                {
-                                    name: 'password',
-                                    placeholder: '',
-                                    type: 'password'
-                                }
-                            ],
-                            buttons: [
-                                {
-                                    text: txt_cancel,
-                                    role: 'cancel',
-                                    handler: data => {
-                                        this.nav.pop();
-                                    }
-                                },
-                                {
-                                    text: txt_enter,
-                                    handler: data => {
-                                        // need error handling
-                                        this.decrypt(data.password, seed)
-                                    }
-                                }
-                            ]
-                        });
-                        alert.present()
-
-                    });
-                });
+    showLoading() {
+        console.log("In show loading")
+        this.translate.get('MESSAGE.LOADING').subscribe((loading: string) => {
+            this.loading = this.loadingCtrl.create({
+                content: loading,
+                dismissOnPageChange: true
             });
-        });
+            this.loading.present();
+        })
     }
 
     showError(message_key, pop = false) {
+        if (this.loading) {
+            this.loading.dismiss();
+        }
         this.translate.get(['MESSAGE.ERROR_TITLE', message_key]).subscribe((translations: any) => {
             let alert = this.alertCtrl.create({
                 title: translations['MESSAGE.ERROR_TITLE'],
