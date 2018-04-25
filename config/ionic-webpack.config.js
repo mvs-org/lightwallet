bf0da11731e58532aaf3eeedf3dfcf38469fed6c
Sven <sven@mutzl.org>
2018-04-25 14:31:27 +0200

    No file rename without production flag

--- config/ionic-webpack.config.js
+++ config/ionic-webpack.config.js
@@ -1,4 +1,4 @@
-var webpack  = require('webpack');
+var webpack = require('webpack');
 const config = require('@ionic/app-scripts/config/webpack.config.js')
 
 // Here we override the dev and prod configs
@@ -17,7 +17,65 @@ const config = require('@ionic/app-scripts/config/webpack.config.js')
 // Then, our html template is updated our overarching build script to be
 // `<script src="main.js?v=xx.xx"></script>`
 //
-config.output.filename = '[name].[chunkhash].js'
+updateEnvironmentFlags();
+if (process.env.IONIC_ENV !== 'dev')
+    config.output.filename = '[name].[chunkhash].js';
 
 // re-export
-module.exports = config
+module.exports = config;
+
+function updateEnvironmentFlags() {
+    String.prototype.capitalizeFirstLetter = function() {
+        return this.charAt(0).toUpperCase() + this.slice(1);
+    };
+
+    process.env.IONIC_FLAVOR = (typeof process.env.IONIC_FLAVOR) != "undefined" ?
+        process.env.IONIC_FLAVOR : getDefaultFlavorForEnv(process.env.IONIC_ENV);
+
+    process.env.IONIC_BUILDTYPE = (typeof process.env.IONIC_BUILDTYPE) != "undefined" ?
+        process.env.IONIC_BUILDTYPE : getDefaultBuildTypeForEnv(process.env.IONIC_ENV);
+
+    process.env.IONIC_VARIANT = (typeof process.env.IONIC_VARIANT) != "undefined" ?
+        process.env.IONIC_VARIANT : getAndroidVariant(process.env.IONIC_BUILDTYPE, process.env.IONIC_FLAVOR);
+
+    console.log("Env is " + process.env.IONIC_ENV);
+    console.log("Build type is " + process.env.IONIC_BUILDTYPE);
+    console.log("Build flavor is " + process.env.IONIC_FLAVOR);
+    console.log("Build variant is " + process.env.IONIC_VARIANT);
+}
+
+function getDefaultBuildTypeForEnv(env_string) {
+    var result = "debug";
+    switch (env_string) {
+        case "prod":
+            result = "release";
+            break;
+        default:
+            result = "debug";
+    }
+    return result;
+}
+
+function getAndroidVariant(buildType, flavor) {
+    var result = flavor + buildType.capitalizeFirstLetter();
+    return result;
+}
+// Get the default "build flavor" if one is not specified.  For dev builds, 
+// this will be "local" -- since just running "ionic serve" you would expect 
+// it to try to connect to a local backend server. For production builds, 
+// it will default to the production flavor as a safeguard in case it is not 
+// explicitly set. For flavors like "dev" or "test" where you are trying to
+// indicate that the app should connect to a shared dev or test backend 
+// server, the build flavor will have to be set explicitly in the 
+// environment.
+function getDefaultFlavorForEnv(env_string) {
+    var result = "local";
+    switch (env_string) {
+        case "prod":
+            result = "production";
+            break;
+        default:
+            result = "local";
+    }
+    return result;
+}
