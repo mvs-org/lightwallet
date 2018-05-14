d1e0994c91cc4d65fceecd0a8e60b90ff799b6e3
Sven <sven@mutzl.org>
2018-05-14 20:51:57 +0200

    Transaction history remove problem functions

--- src/pages/transactions/transactions.ts
+++ src/pages/transactions/transactions.ts
@@ -14,7 +14,6 @@ import { MvsServiceProvider } from '../../providers/mvs-service/mvs-service';
 export class TransactionsPage {
 
     asset: any
-    addresses: string[]
     txs: any[]
     loading: boolean
 
@@ -33,126 +32,50 @@ export class TransactionsPage {
             })
     }
 
-    private orderTxs(txs) {
-        // temporary array holds objects with position and sort-value
-        var mapped = txs.map(function(el, i) {
-            return { index: i, value: -el.timestamp };
-        })
-
-        // sorting the mapped array containing the reduced values
-        mapped.sort(function(a, b) {
-            return +(a.value > b.value) || +(a.value === b.value) - 1;
-        });
-
-        // container for the resulting order
-        var result = mapped.map(function(el) {
-            return txs[el.index];
-        });
-        return result
-    }
-
     private showTxs(filter) {
         return this.mvsServiceProvider.getMvsAddresses()
             .then((addresses: string[]) => {
-                this.addresses = addresses
                 return this.mvsServiceProvider.getMvsTxs()
+                    .then((txs: any[]) => this.filterTxs(txs,filter.symbol, addresses))
             })
-            .then((txs) => this.txsMap(txs))
-            .then((txs) => this.orderTxs(txs))
-            .then((txs) => this.filterTxs(txs, filter))
             .then((txs: any) => {
                 this.txs = txs
                 this.loading = false;
-                return txs
             })
     }
 
-    private filterTxs(txs: any[], filter: any) {
-        return Promise.all(txs.filter((tx) => this.filterTx(tx, filter)))
+    private filterTxs(txs: any[], symbol, addresses) {
+        return Promise.all(txs.filter((tx) => this.filterTx(tx, symbol, addresses)))
     }
 
-    private filterTx(tx: any, filter: any) {
-        if (filter) {
-            if (filter.symbol && filter.symbol === tx.asset.symbol) {
-                return tx
-            } else {
-            }
-        } else {
-            return tx
-        }
-    }
 
-    public txsMap(txs) {
-        return Promise.all(txs.map((tx: any) => {
-            return this.checkSide(tx).then((tx) => this.checkValue(tx));
-        }))
-    }
-
-    public checkValue(tx) {
-        var isMineTXIO = this.isMineTXIO
-        var addresses = this.addresses
-        return new Promise((resolve, reject) => {
-            //Init tx asset
-            tx.asset = {
-                symbol: 'ETP',
-                decimals: 8
-            }
-            return Promise.all(tx.outputs.map((output) =>
-                isMineTXIO(output, addresses).then((txio: any) => {
-                    //TODO: Check for asset transfer quantity
-                    if (txio.asset.symbol !== 'ETP') {
-                        tx.asset = txio.asset
+    private filterTx(tx: any, asset: string, addresses: Array<string>) {
+        let result=false;
+        tx.inputs.forEach((input)=>{
+            if(this.isMineTXIO(input,addresses)){
+            if(input.attachment.type=='asset-transfer'&&input.attachment.symbol==asset)
+                result=true;
+                else if(asset=='ETP' && input.value)
+                    result=true;
                     }
-                    if (tx.side === 'in') {
-                        if (tx.quantity)
-                            tx.quantity += output.quantity
-                        else
-                            tx.quantity = output.quantity
+        });
+        if(result) return tx;
+        tx.outputs.forEach((output)=>{
+            if(this.isMineTXIO(output,addresses)){
+            if(output.attachment.type=='asset-transfer'&&output.attachment.symbol==asset)
+                result=true;
+                else if(asset=='ETP' && output.value)
+                    result=true;
                     }
-                }, (txio: any) => {
-                    if (tx.side === 'out') {
-                        if (txio.asset.symbol !== 'ETP') {
-                            tx.asset = txio.asset
-                        }
-                        if (tx.quantity)
-                            tx.quantity += output.quantity
-                        else
-                            tx.quantity = output.quantity
-                    }
-                }))).then(() => resolve(tx))
-        })
+        });
+        if(result) return tx;
     }
 
     public formatQuantity(quantity, decimals) {
         return quantity / Math.pow(10, decimals)
     }
 
-    private isMineTXIO(txio, addresses) {
-        return new Promise((resolve, reject) => {
-            if (addresses.indexOf(txio.address) !== -1) {
-                resolve(txio)
-            }
-            else {
-                reject(txio)
-            }
-        })
-    }
-
-    public checkSide(tx) {
-        return new Promise((resolve, reject) => {
-            var addresses = this.addresses
-            tx.inputs.forEach((txio) => {
-                if (addresses.indexOf(txio.address) !== -1) {
-                    tx.side = 'out'
-                    resolve(tx)
-                }
-            })
-            if (typeof tx.side === 'undefined') {
-                tx.side = 'in'
-                resolve(tx);
-            }
-        })
-    }
+    private isMineTXIO = (txio, addresses)=>(addresses.indexOf(txio.address) !== -1)
 
     format(quantity, decimals) {
         return quantity / Math.pow(10, decimals);
