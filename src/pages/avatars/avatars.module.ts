45e573404ae240dbd2f859250222bbd25570d937
Laurent Salou <laurent.salou@hotmail.fr>
2018-05-28 19:01:54 +0800

    Update design Avatars page

--- src/pages/avatars/avatars.module.ts
+++ src/pages/avatars/avatars.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { AvatarsPage } from './avatars';
+import { TranslateModule} from '@ngx-translate/core';
 
 @NgModule({
   declarations: [
@@ -8,6 +9,7 @@ import { AvatarsPage } from './avatars';
   ],
   imports: [
     IonicPageModule.forChild(AvatarsPage),
+    TranslateModule
   ],
 })
 export class AvatarsPageModule {}
