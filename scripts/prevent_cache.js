aa503c9ddea304bebe452e003c0bbce911bf429a
Sven <sven@mutzl.org>
2018-06-12 21:47:22 +0200

    Prevent cache

--- scripts/prevent_cache.js
+++ scripts/prevent_cache.js
@@ -3,7 +3,13 @@ var package = require('../package.json');
 
 let version_num=package.version;
 console.info("Add version tag "+version_num);
-var index_orig = fs.readFileSync('./src/index.html', 'utf8');
-var index_new = index_orig.replace("main.js", "main.js?version="+version_num);
-index_new = index_new.replace("main.css", "main.css?version="+version_num);
-fs.writeFileSync('./www/index.html', index_new, 'utf8');
+versionize('./src/index.html', './www/index.html', version_num, ['main.js','main.css','vendor.js','polyfills.js','cordova.js']);
+
+function versionize(src_file, target_file, version, targets){
+	var content = fs.readFileSync(src_file, 'utf8');
+	targets.forEach(target=>{
+		content=content.replace(target, target+"?version="+version);
+	});
+	fs.writeFileSync(target_file, content, 'utf8');
+	return content;
+}
