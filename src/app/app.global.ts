de0f47130c5a6e18a2e20e2b7940ea64af358e2d
Sven <sven@mutzl.org>
2018-07-20 12:05:16 +0200

    Browser 0.3.8

--- src/app/app.global.ts
+++ src/app/app.global.ts
@@ -4,7 +4,7 @@ import { Storage } from '@ionic/storage';
 
 @Injectable()
 export class AppGlobals {
-    readonly version: string = '0.3.7';
+    readonly version: string = '0.3.8';
     readonly db_version = 4;
     readonly name: string = 'Supernova';
     readonly algo: string = 'aes';
