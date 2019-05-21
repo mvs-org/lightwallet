e9798746db694b843014880bafe733d96fa5c044
canguruhh <sven@mutzl.org>
2019-05-21 10:45:25 +0200

    Cleanup account page

--- src/app/account/components/mst-card/mst-card.ts
+++ src/app/account/components/mst-card/mst-card.ts
@@ -1,5 +1,4 @@
 import { Component, Input } from '@angular/core';
-import { Router } from '@angular/router';
 
 @Component({
     selector: 'mst-card',
@@ -14,16 +13,8 @@ export class MSTCardComponent {
     @Input() swap: boolean;
 
     constructor(
-        private router: Router,
     ) {}
 
     errorImg = (e) => e.target.remove();
 
-    gotoTransactions = () => this.router.navigate(['/account/history'], {queryParams: { asset: this.symbol, icon: this.icon }});
-
-    gotoTransfer = () => this.router.navigate(['/account/send'], {queryParams: { asset: this.symbol }});
-
-    gotoReceive = () => this.router.navigate(['/account/addresses'], {queryParams: { asset: this.symbol }});
-
-    gotoSwap = () => this.router.navigate(['/account/swap'], {queryParams: { asset: this.symbol }});
 }
