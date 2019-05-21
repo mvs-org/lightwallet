b423d747199b93e3fa27c8d445b6dc7847fce22e
canguruhh <sven@mutzl.org>
2019-05-21 11:36:24 +0200

    Grid layout repair for button row

--- src/app/account/components/mst-card/mst-card.ts
+++ src/app/account/components/mst-card/mst-card.ts
@@ -2,7 +2,8 @@ import { Component, Input } from '@angular/core';
 
 @Component({
     selector: 'mst-card',
-    templateUrl: 'mst-card.html'
+    templateUrl: 'mst-card.html',
+    styleUrls: ['./mst-card.scss']
 })
 export class MSTCardComponent {
 
