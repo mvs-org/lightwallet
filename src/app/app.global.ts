5e0d5154c6b65e6c009b31ce28e160be31a73519
Laurent Salou <laurent.salou@hotmail.fr>
2019-07-15 14:53:38 +0800

    Update version to 0.7.3

--- src/app/app.global.ts
+++ src/app/app.global.ts
@@ -4,7 +4,7 @@ import { Storage } from '@ionic/storage';
 
 @Injectable()
 export class AppGlobals {
-    readonly version: string = '0.7.2';
+    readonly version: string = '0.7.3';
     readonly db_version = 4;
     readonly name: string = 'Pillars of Creation';
     readonly algo: string = 'aes';
