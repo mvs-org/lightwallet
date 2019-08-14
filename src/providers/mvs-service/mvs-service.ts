12fdd3f4c9059e34c8bd710ab6c32f3a3d33a100
Laurent Salou <laurent.salou@hotmail.fr>
2019-08-14 16:57:13 +0800

    Add DNA logo

--- src/providers/mvs-service/mvs-service.ts
+++ src/providers/mvs-service/mvs-service.ts
@@ -14,8 +14,7 @@ export class MvsServiceProvider {
     DEFAULT_BALANCES = {
         ETP: { frozen: 0, available: 0, decimals: 8 },
         MST: {
-            "PARCELX.GPX": { frozen: 0, available: 0, decimals: 8 },
-            "RIGHTBTC.RT": { frozen: 0, available: 0, decimals: 4 },
+            "DNA": { frozen: 0, available: 0, decimals: 4 },
             "MVS.ZGC": { frozen: 0, available: 0, decimals: 8 },
             "MVS.ZDC": { frozen: 0, available: 0, decimals: 6 },
             "SDG": { frozen: 0, available: 0, decimals: 8 }
