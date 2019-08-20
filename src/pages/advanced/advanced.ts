72f80061923cf3a1838fa77cf60d172cf5076af8
Laurent Salou <laurent.salou@hotmail.fr>
2019-08-20 19:12:44 +0800

    Integrate bitident library

--- src/pages/advanced/advanced.ts
+++ src/pages/advanced/advanced.ts
@@ -1,10 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, Platform } from 'ionic-angular';
-import { MvsServiceProvider } from '../../providers/mvs-service/mvs-service';
 import { TranslateService } from '@ngx-translate/core';
 import { AppGlobals } from '../../app/app.global';
-import { WalletServiceProvider } from '../../providers/wallet-service/wallet-service';
-import { AlertProvider } from '../../providers/alert/alert';
 
 @IonicPage()
 @Component({
@@ -13,9 +10,7 @@ import { AlertProvider } from '../../providers/alert/alert';
 })
 export class AdvancedPage {
 
-    connectcode: any;
-    network: string;
-    saved_accounts_name: any = [];
+    network: string
 
     constructor(
         public nav: NavController,
@@ -37,7 +32,5 @@ export class AdvancedPage {
     DecodeTxPage = e => this.nav.push("DecodeTxPage")
 
     EthBridgePage = e => this.nav.push("EthBridgePage")
-
-    InformationPage = e => this.nav.push("InformationPage")
 
 }
--- src/pages/auth/auth.ts
+++ src/pages/auth/auth.ts
@@ -7,6 +7,7 @@ import { BarcodeScanner } from '@ionic-native/barcode-scanner';
 import { WalletServiceProvider } from '../../providers/wallet-service/wallet-service';
 import { AuthServiceProvider } from '../../providers/auth-service/auth-service';
 import { AlertProvider } from '../../providers/alert/alert';
+import { Request } from 'bitident';
 
 @IonicPage()
 @Component({
@@ -24,6 +25,7 @@ export class AuthPage {
     no_avatar: boolean = false
     passphrase: string = ''
     verifiedMessage: any
+    network: string
 
     leftTime: number = 0;
 
@@ -42,6 +44,7 @@ export class AuthPage {
         this.qrCodeLoaded = false;
         this.isApp = (!document.URL.startsWith('http') || document.URL.startsWith('http://localhost:8080'));
         this.loadAvatars();
+        this.network = this.globals.network
 
     }
 
@@ -68,35 +71,19 @@ export class AuthPage {
     validPassword = (passphrase) => (passphrase.length > 0)
 
     scan() {
-        let wallet = {};
-        this.translate.get(['SCANNING.MESSAGE_ACCOUNT']).subscribe((translations: any) => {
+        this.translate.get(['SCANNING.AUTH_MESSAGE']).subscribe((translations: any) => {
             this.barcodeScanner.scan(
                 {
                     preferFrontCamera: false, // iOS and Android
                     showFlipCameraButton: false, // iOS and Android
                     showTorchButton: false, // iOS and Android
                     torchOn: false, // Android, launch with the torch switched on (if available)
-                    prompt: translations['SCANNING.MESSAGE_ACCOUNT'], // Android
+                    prompt: translations['SCANNING.AUTH_MESSAGE'], // Android
                     resultDisplayDuration: 0, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
                     formats: "QR_CODE", // default: all but PDF_417 and RSS_EXPANDED
                 }).then((result) => {
                     if (!result.cancelled) {
-                        console.log(result.text.toString())
-                        let content = result.text.toString().split('&')
-                        if (content.length != 3) {
-                            this.alert.showError('IMPORT_QRCODE', '')
-                        } else {
-                            this.globals.getNetwork()
-                                .then((network) => {
-                                    if (content[1] != network.charAt(0)) {
-                                        this.alert.showError('MESSAGE.NETWORK_MISMATCH', '')
-                                    } else {
-                                        wallet = { "index": Math.max(5, Math.min(parseInt(content[2]), 50)) }
-                                        this.wallet.setWallet(wallet)
-                                        this.wallet.setMobileWallet(content[0]).then(() => this.qrCodeLoaded = true)
-                                    }
-                                })
-                        }
+                        this.check(result.text.toString())
                     }
                 })
         })
@@ -106,9 +93,13 @@ export class AuthPage {
 
         this.alert.showLoading()
         try {
-            let obj = JSON.parse(message)
 
-            if(obj.type != 'd2fa') {
+            let json = JSON.stringify(Request.decode(message))
+            let obj = JSON.parse(json)
+
+            if (obj.network !== this.network) {
+            this.alert.showError('MESSAGE.AUTH_DIFFERENT_NETWORK', obj.network);
+            } else if(obj.type != 'auth') {
                 this.alert.showError('MESSAGE.AUTH_TYPE_NOT_SUPPORTED', obj.type);
             } else if (this.avatars.indexOf(obj.target) === -1) {
                 this.alert.showError('MESSAGE.AUTH_UNKNOWN_AVATAR', obj.target);
@@ -116,17 +107,18 @@ export class AuthPage {
                 this.alert.showError('MESSAGE.AUTH_TIMEOUT', '');
             } else {
 
-                let sig = obj.signature
-                delete obj.signature
-
                 let sourceAvatar = await this.mvs.getGlobalAvatar(obj.source)
 
-                if(!this.wallet.verifyMessage(JSON.stringify(this.sortObject(obj)), sourceAvatar.address, sig)) {
-                    this.alert.showError('MESSAGE.AUTH_WRONG_SIGNATURE', obj.source);
+                let {sourceSignature, ...pureRequest} = obj
+
+                this.wallet.verifyMessage(new Request(pureRequest).encode('hex'), sourceAvatar.address, sourceSignature)
+
+                if(!this.wallet.verifyMessage(JSON.stringify(this.sortObject(pureRequest)), sourceAvatar.address, sourceSignature)) {
+                    this.alert.showError('MESSAGE.AUTH_WRONG_SIGNATURE', pureRequest.source);
                 } else {
-                    obj.hostname = new URL(obj.callback).hostname
-                    this.verifiedMessage = obj;
-                    this.leftTime = (obj.time + obj.timeout) - (Math.floor(Date.now())/1000)
+                    pureRequest.hostname = new URL(pureRequest.callback).hostname
+                    this.verifiedMessage = pureRequest;
+                    this.leftTime = (pureRequest.time + pureRequest.timeout) - (Math.floor(Date.now())/1000)
                 }
             }
         } catch (e) {
@@ -161,15 +153,27 @@ export class AuthPage {
                 }
             })
             .catch((error) => {
-                console.error(error.message)
                 this.alert.stopLoading()
-                switch(error.message){
-                    case "ERR_DECRYPT_WALLET":
-                        this.alert.showError('MESSAGE.PASSWORD_WRONG', '')
-                        break
-                    default:
-                        this.alert.showError('MESSAGE.AUTH_SIGN', error.message)
-                        throw Error('ERR_D2FA')
+                if(error.message) {
+                    console.error(error.message)
+                    switch(error.message){
+                        case "ERR_DECRYPT_WALLET":
+                            this.alert.showError('MESSAGE.PASSWORD_WRONG', '')
+                            break
+                        default:
+                            this.alert.showError('MESSAGE.AUTH_SIGN', error.message)
+                            throw Error('ERR_D2FA')
+                    }
+                } else {
+                    console.error(error._body)
+                    switch(error._body){
+                        case "ERR_EXPIRED":
+                            this.alert.showError('MESSAGE.AUTH_EXPIRED', '')
+                            break
+                        default:
+                            this.alert.showError('MESSAGE.AUTH_SEND_SIG_ERROR', error.message)
+                            throw Error('ERR_D2FA')
+                    }
                 }
             })
     }
