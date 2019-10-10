70e2099bce82bfb67f5df24f1c4ca75a7db146a7
Laurent Salou <laurent.salou@hotmail.fr>
2019-10-10 15:55:24 +0800

    Display locked MST as locked right after being locked (Issue #208)

--- src/providers/mvs-service/mvs-service.ts
+++ src/providers/mvs-service/mvs-service.ts
@@ -773,6 +773,38 @@ export class MvsServiceProvider {
                             output.attachment.type = 'asset-transfer';
                             if (balances && balances.MST && balances.MST[output.attachment.symbol])
                                 output.attachment.decimals = balances.MST[output.attachment.symbol].decimals
+                            if(output.attenuation) {
+                                let attenuationObject = {}
+                                const attenuationArray = output.attenuation.model.split(';')
+                                attenuationArray.forEach(param => {
+                                    const temp = param.split('=')
+                                    attenuationObject[temp[0]] = temp[1]
+                                })
+                                output.attenuation_model_param = {
+                                    current_period_nbr: parseInt(attenuationObject['PN']),
+                                    lock_period: parseInt(attenuationObject['LP']),
+                                    lock_quantity: parseInt(attenuationObject['LQ']),
+                                    next_interval: parseInt(attenuationObject['LH']),
+                                    total_period_nbr: parseInt(attenuationObject['UN']),
+                                    type: parseInt(attenuationObject['TYPE']),
+                                }
+                                if(attenuationObject['IR']) {
+                                    output.attenuation_model_param.inflation_rate = parseInt(attenuationObject['IR'])
+                                }
+                                if(attenuationObject['UC'] && attenuationObject['UQ']) {
+                                    const numbers = attenuationObject['UC'].split(',')
+                                    const quantities = attenuationObject['UQ'].split(',')
+                                    let locked = []
+                                    for(let i=0; i < numbers.length; i++) {
+                                        locked.push({
+                                            number: parseInt(numbers[i]),
+                                            quantity: parseInt(quantities[i])
+                                        })
+                                    }
+                                    output.attenuation_model_param.locked = locked
+                                }
+                                output.height = tx.height
+                            }
                             break;
                     }
                     break;
