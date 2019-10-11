c69c73738c976373502876b53cd9d57ead238e9c
Laurent Salou <laurent.salou@hotmail.fr>
2019-10-11 15:06:05 +0800

    Unify design of export xpub page with other pages

--- src/pages/export-xpub/export-xpub.module.ts
+++ src/pages/export-xpub/export-xpub.module.ts
@@ -2,20 +2,22 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { ExportXpubPage } from './export-xpub';
 import { AlertProvider } from '../../providers/alert/alert';
-import { TranslateModule} from '@ngx-translate/core';
+import { TranslateModule } from '@ngx-translate/core';
 import { ClipboardModule } from 'ngx-clipboard/dist';
+import { ElasticModule } from 'angular2-elastic';
 
 @NgModule({
-  declarations: [
-    ExportXpubPage,
-  ],
-  providers: [
-    AlertProvider
-  ],
-  imports: [
-    IonicPageModule.forChild(ExportXpubPage),
-    TranslateModule,
-    ClipboardModule,
-  ],
+    declarations: [
+        ExportXpubPage,
+    ],
+    imports: [
+        IonicPageModule.forChild(ExportXpubPage),
+        TranslateModule,
+        ClipboardModule,
+        ElasticModule,
+    ],
+    providers: [
+        AlertProvider
+    ],
 })
 export class ExportXpubPageModule { }
