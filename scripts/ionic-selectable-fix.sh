b0beb983ffefd52be2ac42a51d01bc7b037dc00e
Laurent Salou <laurent.salou@hotmail.fr>
2020-04-14 14:51:14 +0200

    Remove previous files

--- scripts/ionic-selectable-fix.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-FILE=node_modules/ionic-selectable/ionic-selectable.metadata.json
-
-if test -f "$FILE"; then
-        echo "found ionic-selectable plugin -- apply fix to build for production"
-	sed -ie 's/"version":4/"version":3/g' $FILE
-fi
-
-
