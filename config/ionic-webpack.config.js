bf7807a6aeb2b2acd0d015b11ba8c141338db8c9
Sven <sven@mutzl.org>
2020-06-01 09:22:19 +0200

    Fresh ionic project

--- config/ionic-webpack.config.js
+++
@@ -1,6 +0,0 @@
-var webpack = require('webpack');
-const config = require('@ionic/app-scripts/config/webpack.config.js')
-
-config.prod.output.filename = '[name].[chunkhash].js';
-
-module.exports = config;
--- karma.conf.js
+++ karma.conf.js
@@ -1,61 +1,31 @@
-var webpackConfig = require('./test-config/webpack.test.js');
+// Karma configuration file, see link for more information
+// https://karma-runner.github.io/1.0/config/configuration-file.html
 
-module.exports = function(config) {
-    var _config = {
-        basePath: './',
-
-        frameworks: ['jasmine'],
-
-        files: [{
-                pattern: './test-config/karma-test-shim.js',
-                watched: true
-            },
-            {
-                pattern: './src/assets/**/*',
-                watched: false,
-                included: false,
-                served: true,
-                nocache: false
-            }
-        ],
-
-        proxies: {
-            '/assets/': '/base/src/assets/'
-        },
-
-        preprocessors: {
-            './test-config/karma-test-shim.js': ['webpack', 'sourcemap']
-        },
-
-        webpack: webpackConfig,
-
-        webpackMiddleware: {
-            stats: 'errors-only'
-        },
-
-        webpackServer: {
-            noInfo: true
-        },
-
-        browserConsoleLogOptions: {
-            level: 'log',
-            format: '%b %T: %m',
-            terminal: true
-        },
-
-        coverageIstanbulReporter: {
-            reports: ['html', 'lcovonly'],
-            fixWebpackSourcePaths: true
-        },
-
-        reporters: config.coverage ? ['kjhtml', 'spec', 'coverage-istanbul'] : ['kjhtml', 'spec'],
-        port: 9876,
-        colors: true,
-        logLevel: config.LOG_INFO,
-        autoWatch: true,
-        browsers: ['ChromeHeadless', 'FirefoxHeadless'],
-        singleRun: true
-    };
-
-    config.set(_config);
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage-istanbul-reporter'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+    client: {
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
+    },
+    coverageIstanbulReporter: {
+      dir: require('path').join(__dirname, '../coverage'),
+      reports: ['html', 'lcovonly', 'text-summary'],
+      fixWebpackSourcePaths: true
+    },
+    reporters: ['progress', 'kjhtml'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['Chrome'],
+    singleRun: false
+  });
 };
