6c658e49adb4ef02fe21b3c4ac5a3486d7fb9561
Sven <sven@mutzl.org>
2020-06-04 12:58:10 +0200

    Decimals pipe

---
+++ src/app/pipes/decimals.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { DecimalsPipe } from './decimals.pipe';
+
+describe('DecimalsPipe', () => {
+  it('create an instance', () => {
+    const pipe = new DecimalsPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
--- src/app/pipes/pipes.module.ts
+++ src/app/pipes/pipes.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormatPipe } from './format.pipe';
+import { DecimalsPipe } from './decimals.pipe';
 
 
 
 @NgModule({
-  declarations: [FormatPipe],
+  declarations: [FormatPipe, DecimalsPipe],
   imports: [
     CommonModule
   ],
   exports: [
-    FormatPipe
+    FormatPipe,
+    DecimalsPipe
   ]
 })
 export class PipesModule { }
