98ab4138581691dfd0687b71c1d60685bcd34f9a
Sven <sven@mutzl.org>
2020-06-16 10:48:39 +0200

    guard for public pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core'
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router'
 import { AccountGuard } from './account.guard'
+import { PublicGuard } from './public.guard'
 
 const routes: Routes = [
     {
@@ -10,6 +11,8 @@ const routes: Routes = [
     },
     {
         path: 'login',
+        canActivate: [PublicGuard],
+        canActivateChild: [PublicGuard],
         loadChildren: () => import('./login/login.module').then(m => m.LoginPageModule)
     },
     {
