571bab164d890f99a5bd20ac7bfe1cf09af44855
canguruhh <sven@mutzl.org>
2020-09-16 17:33:05 +0200

    service worker update notification

--- src/app/services/sw.service.ts
+++ src/app/services/sw.service.ts
@@ -1,14 +1,21 @@
 import { Injectable, ApplicationRef } from '@angular/core'
-import { SwUpdate, SwRegistrationOptions } from '@angular/service-worker'
+import { SwUpdate, } from '@angular/service-worker'
 import { first, } from 'rxjs/operators'
 import { interval, concat } from 'rxjs'
+import { ToastService } from './toast.service'
+import { TranslateService } from '@ngx-translate/core'
 
 @Injectable({
   providedIn: 'root'
 })
 export class SwService {
 
-  constructor(public appRef: ApplicationRef, updates: SwUpdate, options: SwRegistrationOptions) {
+  constructor(
+    public appRef: ApplicationRef,
+    updates: SwUpdate,
+    toastService: ToastService,
+    transalte: TranslateService,
+  ) {
 
     // Allow the app to stabilize first, before starting polling for updates with `interval()`.
     const appIsStable$ = appRef.isStable.pipe(first(isStable => isStable === true))
@@ -21,12 +28,17 @@ export class SwService {
       console.log(`update activated. updated from ${event.previous} to ${event.current}`)
     })
 
-    updates.available.subscribe(event => {
+    updates.available.subscribe(async (event) => {
       console.log(`new update available`)
       // TODO ask the user for confirmation
-      if (true) {
-        updates.activateUpdate().then(() => document.location.reload())
-      }
+      const [txtUpdateAvailable, txtUpdateApply] = await transalte.get(['UPDATE.AVAILABLE_TEXT', 'UPDATE.APPLY']).toPromise()
+      toastService.toastController.create({
+        message: txtUpdateAvailable, buttons: [{
+          text: txtUpdateApply, handler: () => {
+            updates.activateUpdate().then(() => document.location.reload())
+          }
+        }]
+      })
     })
   }
 
