191aa64b43fa8a05d1376dee345c5dc0ceca089f
Laurent Salou <laurent.salou@hotmail.fr>
2021-04-17 11:13:03 +0200

    Udpate version to 0.10.5

--- src/app/services/app.service.ts
+++ src/app/services/app.service.ts
@@ -7,7 +7,7 @@ import { Subject, BehaviorSubject } from 'rxjs'
 })
 export class AppService {
 
-  readonly version: string = '0.10.3'
+  readonly version: string = '0.10.5'
   readonly ci_number = ''
   readonly ci_link = ''
   readonly ci_commit = ''
