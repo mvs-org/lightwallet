7ba8be3b65edee9727e192b8d9204a7625bef551
ChiefWf2772 <chiefwhitefeather@whitebuffalo.fans>
2022-06-13 17:32:10 +0000

    merge

---
+++ wb-alchemy/draconian/src/main/java/whitebuffalo/fans/App.java
@@ -0,0 +1,17 @@
+package whitebuffalo.fans;
+
+/**
+ * Hello world!
+ */
+public final class App {
+    private App() {
+    }
+
+    /**
+     * Says hello to the world.
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
